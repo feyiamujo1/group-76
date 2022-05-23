b2edea5c43d2500d8f01f95235e00d4d514a393e
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2022-05-23 15:06:30 +0100

    fixed some issues with the data being displayed

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -3,14 +3,14 @@ import "./client.css"
 import axios from 'axios'
 
 function Clients() {
-  const [client, setClient] = useState([])
+  const [clients, setClient] = useState([])
   const [statusNote, setStatusNote] = useState("")
   const url = 'https://fakerapi.it/api/v1/companies?_quantity=1'
 
   useEffect(() => {
     axios.get(url)
       .then(response => {
-        console.log(response)
+        
         if (response.status === 200) {
           setStatusNote("")
           setClient(response.data.data)
@@ -20,7 +20,8 @@ function Clients() {
       })
 
   }, [])
-  console.log(client)
+  console.log("This is client")
+  console.log(clients)
 
   return (
     <div className='clients'>
@@ -32,27 +33,26 @@ function Clients() {
         <table className='clients_table'>
           <tr className='clients_table'>
             <th>Id</th>
-            <th className='clients_th'>Full name</th>
+            <th className='clients_th'>Client</th>
             <th className='clients_th'>Email</th>
             <th className='clients_th'>Phone number</th>
             <th className='clients_th'>Gender</th>
-            <th className='clients_th'>Images</th>
+            {/* <th className='clients_th'>Images</th> */}
           </tr>
           {
-            client ? client.map((client, id) =>
+            clients ? clients.map((client, id ) => (
               <tr className='client_table_tr'>
-                
-                  <td>{client.id}</td>
-                  <td>{client.name}</td>
-                  <td>{client.email}</td>
-                  <td>{client.phone}</td>
-                  <td>male</td>
-                  <td className='clients_card_image'>
-                    <img src={client.image} alt="client"/></td>
-                  
-              </tr>
-            )
-              : null
+              <td>{id + 1}</td>
+              <td className='clients_card_image'>
+                  <img src={client["contact"].image} alt="client"/>
+                  <span>{client["contact"].firstname} {client["contact"].lastname}</span>
+                </td>
+                <td>{client["contact"].email}</td>
+                <td>{client["contact"].phone}</td>
+                <td>{client["contact"].gender}</td> 
+            </tr>
+            ) )
+            : null
           }
         </table>
       </div>
